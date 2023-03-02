006a507855ce4c33112dc4a249bc97b87e72d116
Pham Van Dung <dung18j@gmail.com>
2023-03-02 19:30:54 +0700

    run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+qemu-system-arm -M versatilepb -kernel output/images/zImage -dtb output/images/versatile-pb.dtb -drive file=output/images/rootfs.ext2,if=scsi,format=raw -append "rootwait root=/dev/sda console=ttyAMA0,115200" -nographic
